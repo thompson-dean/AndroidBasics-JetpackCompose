32ffb0f03dd61c8dedaa73028ff3da2fa7334bc0
Dean Thompson <deanthompson.music@gmail.com>
2023-02-28 21:51:20 +0900

    first commit

---
+++ ArtSpace/app/src/main/java/com/example/artspace/DataManager.kt
@@ -0,0 +1,32 @@
+package com.example.artspace
+
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+
+class DataManager {
+
+    data class Item(
+        @DrawableRes val image: Int,
+        @StringRes val imageName: Int,
+        @StringRes val imageCapturedBy: Int
+    )
+
+    val dataArray: Array<Item> = arrayOf(
+        Item(
+            image = R.drawable.snow_monkey,
+            imageName = R.string.snow_monkey,
+            imageCapturedBy = R.string.dean
+        ),
+        Item(
+            image = R.drawable.lake,
+            imageName = R.string.lake,
+            imageCapturedBy = R.string.waldo
+        ),
+        Item(
+            image = R.drawable.frozen_grass,
+            imageName = R.string.frozen_grass,
+            imageCapturedBy = R.string.jazz
+        )
+    )
+
+}
